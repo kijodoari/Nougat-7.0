9604d5227488f9e0aa9badda47514d51e7bebddc
The-Sickness <alloycowboy6969@gmail.com>
2017-08-01 13:49:57 -0500

    Added a few things to scripts

--- build_kernel/G920T/ramdisk/sbin/sick.sh
+++ build_kernel/G920T/ramdisk/sbin/sick.sh
@@ -32,7 +32,7 @@ fi;
 #####################################################################
 # Make Kernel Data Path
 
-if [ ! -d /data/.quintz ]; then
+if [ ! -d /data/.sick ]; then
 	$BB mkdir -p /data/.sick;
 	$BB chmod -R 0777 /.sick/;
 else
@@ -88,13 +88,103 @@ fi;
 # Kernel custom test
 
 if [ -e /data/.sick/Kernel-test.log ]; then
-	rm /data/.quintz/Kernel-test.log;
+	rm /data/.sick/Kernel-test.log;
 fi;
 echo  Kernel script is working !!! >> /data/.sick/Kernel-test.log;
 echo "excecuted on $(date +"%d-%m-%Y %r" )" >> /data/.sick/Kernel-test.log;
 
 
 ####################################################################
+
+#####################################################################
+# Default DVFS Governor
+echo "interactive" > /sys/devices/14ac0000.mali/dvfs_governor;
+
+#####################################################################
+# GPU CLOCK
+echo "100" > /sys/devices/platform/gpusysfs/gpu_min_clock;
+echo "852" > /sys/devices/platform/gpusysfs/gpu_max_clock;
+
+#####################################################################
+# Default TCP Congestion Controller
+
+echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control;
+
+#####################################################################
+# Arch Power
+
+echo "0" > /sys/kernel/sched/arch_power;
+
+#####################################################################
+# Gentle Fair Sleepers
+
+echo "0" > /sys/kernel/sched/gentle_fair_sleepers;
+
+#####################################################################
+
+# KNOX Remover
+cd /system;
+rm -rf *app/BBCAgent*;
+rm -rf *app/Bridge*;
+rm -rf *app/ContainerAgent*;
+rm -rf *app/ContainerEventsRelayManager*;
+rm -rf *app/DiagMonAgent*;
+rm -rf *app/ELMAgent*;
+rm -rf *app/FotaClient*;
+rm -rf *app/FWUpdate*;
+rm -rf *app/FWUpgrade*;
+rm -rf *app/HLC*;
+rm -rf *app/KLMSAgent*;
+rm -rf *app/*Knox*;
+rm -rf *app/*KNOX*;
+rm -rf *app/LocalFOTA*;
+rm -rf *app/RCPComponents*;
+rm -rf *app/SecKids*;
+rm -rf *app/SecurityLogAgent*;
+rm -rf *app/SPDClient*;
+rm -rf *app/SyncmlDM*;
+rm -rf *app/UniversalMDMClient*;
+rm -rf container/*Knox*;
+rm -rf container/*KNOX*;
+cd /;
+
+#####################################################################
+# Fixing Permissions
+
+$BB chown -R system:system /data/anr;
+$BB chown -R root:root /tmp;
+$BB chown -R root:root /res;
+$BB chown -R root:root /sbin;
+$BB chown -R root:root /lib;
+$BB chmod -R 777 /tmp/;
+$BB chmod -R 775 /res/;
+$BB chmod -R 06755 /sbin/ext/;
+$BB chmod -R 0777 /data/anr/;
+$BB chmod -R 0400 /data/tombstones;
+$BB chown -R root:root /data/property;
+$BB chmod -R 0700 /data/property;
+$BB chmod 06755 /sbin/busybox;
+$BB chmod 06755 /system/xbin/busybox;
+
+#####################################################################
+
+#####################################################################
+# Google Services battery drain fixer by Alcolawl@xda
+
+	# http://forum.xda-developers.com/google-nexus-5/general/script-google-play-services-battery-t3059585/post59563859
+#	sleep 60
+#	pm enable com.google.android.gms/.update.SystemUpdateActivity
+#	pm enable com.google.android.gms/.update.SystemUpdateService
+#	pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+#	pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
+#	pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+#	pm enable com.google.android.gsf/.update.SystemUpdateActivity
+#	pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
+#	pm enable com.google.android.gsf/.update.SystemUpdateService
+#	pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
+#	pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+
+#####################################################################
 
 #####################################################################
 $BB mount -t rootfs -o remount,ro rootfs
